77e409f3528b52c009a940a975676115d2216794
Johnny Lay <johnny.lay.developer@gmail.com>
2021-12-11 20:12:11 +0100

    Created a new model schema for User

---
+++ server/models/user_model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true, trim: true },
+  last_name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, trim: true },
+  password: { type: String, required: true, trim: true },
+  search_results: [
+    {
+      type: new Schema({
+        from: {
+          id: { type: String, trim: true },
+          extId: { type: String, trim: true },
+          name: { type: String, trim: true },
+          products: { type: Number, trim: true },
+        },
+        to: {
+          id: { type: String, trim: true },
+          extId: { type: String, trim: true },
+          name: { type: String, trim: true },
+          products: { type: Number, trim: true },
+        },
+        created_at: { type: Date, default: Date.now },
+      }),
+    },
+  ],
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
