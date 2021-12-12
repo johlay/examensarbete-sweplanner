af455ea5a7969e8f32f93201b472d1920b3edb66
Johnny Lay <johnny.lay.developer@gmail.com>
2021-12-12 19:33:07 +0100

    Added a new router for routeplanner

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.get("/", (req, res) => res.sendStatus(200));
 
 router.use("/user", require("./user_router"));
+router.use("/routeplanner", require("./routeplanner_router"));
 
 module.exports = router;
---
+++ server/routes/routeplanner_router.js
@@ -0,0 +1,6 @@
+const routeplannerController = require("../controllers/routeplanner_controller");
+
+const express = require("express");
+const router = express.Router();
+
+module.exports = router;
