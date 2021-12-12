3695d86e73b5008266e433519f8da2b940cf00c3
Johnny Lay <johnny.lay.developer@gmail.com>
2021-12-12 19:57:40 +0100

    Added new environment variable

--- server/config.js
+++ server/config.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 module.exports = {
   port: process.env.PORT || 3000,
   db_connection: process.env.MONGO_DB_CONNECTION,
+  resrobot_api_key: process.env.RESROBOT_API_KEY,
   jwt_access_token_secret: process.env.JWT_ACCESSS_TOKEN_SECRET,
   jwt_access_token_expires: process.env.JWT_ACCESS_TOKEN_EXPIRES || "10h",
   password_hash_saltrounds: parseInt(process.env.PASSWORD_HASH_SALTROUNDS) || 8,
