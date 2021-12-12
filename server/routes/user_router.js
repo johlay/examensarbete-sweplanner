56e684e3326d6a4a99fcd86a01bebeeda3bfa3fd
Johnny Lay <johnny.lay.developer@gmail.com>
2021-12-12 18:13:21 +0100

    Implemented access token verification for user's search results route

--- server/routes/user_router.js
+++ server/routes/user_router.js
@@ -1,9 +1,11 @@
 const userController = require("../controllers/user_controller");
+const { verifyJwtAccessToken } = require("../middlewares/");
 
 const express = require("express");
 const router = express.Router();
 
 router.post("/login", userController.login);
 router.post("/register", userController.register);
+router.put("/search-results", [verifyJwtAccessToken]);
 
 module.exports = router;
