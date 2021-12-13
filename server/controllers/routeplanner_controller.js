12c15768f4b475eb8a876731a6767ed1ca775b42
Johnny Lay <johnny.lay.developer@gmail.com>
2021-12-13 16:14:25 +0100

    Added route for looking up stops

--- server/controllers/routeplanner_controller.js
+++ server/controllers/routeplanner_controller.js
@@ -1,1 +1,29 @@
-module.exports = {};
+/**
+ * Routeplanner controller is using ResRobot's API - an API from Trafiklab
+ */
+
+const axios = require("axios");
+const { resrobot_api_key } = require("../config");
+
+// ResRobot Stop lookup - provides information about stops, including their ids and position, by searching based on their name.
+const routeplanner_stop_lookup_get = async (req, res) => {
+  const { location } = req.query;
+
+  try {
+    const response = await axios(
+      `https://api.resrobot.se/v2/location.name?input=${location}&format=json&key=${resrobot_api_key}`
+    );
+
+    const data = await response.data;
+
+    if (!data) {
+      throw Error("Unsuccessful when retrieving data");
+    }
+
+    res.status(200).json({ status: "success", data: data });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
+
+module.exports = { routeplanner_stop_lookup_get };
--- server/routes/routeplanner_router.js
+++ server/routes/routeplanner_router.js
@@ -3,4 +3,6 @@ const routeplannerController = require("../controllers/routeplanner_controller")
 const express = require("express");
 const router = express.Router();
 
+router.get("/stop-lookup", routeplannerController.routeplanner_stop_lookup_get);
+
 module.exports = router;
