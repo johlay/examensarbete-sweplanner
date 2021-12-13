56a2c1c3a11b1eb8a38235f3a7fc41073178001e
Johnny Lay <johnny.lay.developer@gmail.com>
2021-12-13 16:37:57 +0100

    Implemented token verification for Routeplanner

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
+const { verifyJwtAccessToken } = require("../middlewares/");
+
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => res.sendStatus(200));
 
 router.use("/user", require("./user_router"));
-router.use("/routeplanner", require("./routeplanner_router"));
+router.use("/routeplanner", [verifyJwtAccessToken], require("./routeplanner_router"));
 
 module.exports = router;
