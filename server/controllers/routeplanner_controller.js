9e0428c6b4e5bf2a5d80bda8244bfdfc15e5974e
Johnny Lay <johnny.lay.developer@gmail.com>
2021-12-13 16:31:39 +0100

    Added route for using routeplanner

--- server/controllers/routeplanner_controller.js
+++ server/controllers/routeplanner_controller.js
@@ -5,6 +5,29 @@
 const axios = require("axios");
 const { resrobot_api_key } = require("../config");
 
+// ResRobot Routeplanner - provides routes between two stops or points
+const routeplanner_routes_get = async (req, res) => {
+  const { date, time, originId, destId } = req.query;
+
+  try {
+    const response = await axios.get(
+      `https://api.resrobot.se/v2/trip?format=json&date=${date}&time=${time}&originId=${originId}&destId=${destId}&lang=en&key=${resrobot_api_key}`
+    );
+
+    const data = await response.data;
+
+    if (!data) {
+      throw Error(
+        "Unsuccessful when retrieving data for: 'Routeplanner's routes'"
+      );
+    }
+
+    res.status(200).json({ status: "success", data: data });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
+
 // ResRobot Stop lookup - provides information about stops, including their ids and position, by searching based on their name.
 const routeplanner_stop_lookup_get = async (req, res) => {
   const { location } = req.query;
@@ -17,7 +40,7 @@ const routeplanner_stop_lookup_get = async (req, res) => {
     const data = await response.data;
 
     if (!data) {
-      throw Error("Unsuccessful when retrieving data");
+      throw Error("Unsuccessful when retrieving data for: 'stop lookup'");
     }
 
     res.status(200).json({ status: "success", data: data });
@@ -26,4 +49,4 @@ const routeplanner_stop_lookup_get = async (req, res) => {
   }
 };
 
-module.exports = { routeplanner_stop_lookup_get };
+module.exports = { routeplanner_routes_get, routeplanner_stop_lookup_get };
--- server/routes/routeplanner_router.js
+++ server/routes/routeplanner_router.js
@@ -4,5 +4,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/stop-lookup", routeplannerController.routeplanner_stop_lookup_get);
+router.get("/routes", routeplannerController.routeplanner_routes_get);
 
 module.exports = router;
