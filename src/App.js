567551aa26d62d60dcb9bfd664dafdf35dd7764c
Johnny Lay <johnny.lay.developer@gmail.com>
2021-12-14 14:36:03 +0100

    Added background color

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/partials/Navigation";
 
 function App() {
+  // change background color for the whole app.
+  document.body.className = "bg-secondary";
+
   return (
     <BrowserRouter>
       <div className="App">
