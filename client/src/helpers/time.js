14988ded20b926252810af0a7d4c63b581ac7a9a
Johnny Lay <johnny.lay.developer@gmail.com>
2021-12-30 09:20:04 +0100

    Code refactoring for rounding time hour

--- client/src/helpers/time.js
+++ client/src/helpers/time.js
@@ -1,13 +1,19 @@
 // check and round HH
 const roundTimeHour = (hour, minute) => {
-  // if HH is 23 and minute is between :45:59 - :59:59 --> return "00"
+  // if HH is 23 and MM is between HH:45:59 - HH:59:59 --> return "00"
   if (hour >= 23 && minute >= 45 && minute < 60) return "00";
 
-  // if time is between HH:00:00 - HH:44:59 --> return same hour
-  if (minute >= 0 && minute < 45) return hour;
+  // if HH is 9 and MM is between HH:45:59 - HH:59:59 --> return next upcoming hour
+  if (hour === 9 && minute >= 45 && minute < 60) return "10";
 
-  // if time is between HH:45:00 - HH:59:59 --> return next upcoming hour
-  if (minute >= 45 && minute < 60) return hour + 1;
+  // if HH is 9 and MM is between HH:00:00 - HH:44:59 --> return same hour
+  if (hour === 9 && minute >= 0 && minute < 45) return "09";
+
+  // if HH is not 9 and MM is between HH:00:00 - HH:44:59 --> return same hour
+  if (hour !== 9 && minute >= 0 && minute < 45) return String(hour);
+
+  // // if HH is not 9 and MM is between HH:45:00 - HH:59:59 --> return next upcoming hour
+  if (hour !== 9 && minute >= 45 && minute < 60) return String(hour + 1);
 };
 
 // check and round :MM of timestamp to nearest "15 minutes"
