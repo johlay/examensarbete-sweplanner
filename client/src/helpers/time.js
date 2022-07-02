0ce482cc2f8ea88a0a631cfe9bea13aa383e8cdf
Johnny Lay <johnny.lay.developer@gmail.com>
2022-07-02 15:01:00 +0200

    Bug fixing for time prefix(HH) when selecting time options

--- client/src/helpers/time.js
+++ client/src/helpers/time.js
@@ -3,6 +3,20 @@ const roundTimeHour = (hour, minute) => {
   // if HH is 23 and MM is between HH:45:59 - HH:59:59 --> return "00"
   if (hour >= 23 && minute >= 45 && minute < 60) return "00";
 
+  // if HH is 0 and MM is between HH:00:00 - HH:45:00 --> return "00"
+  if (hour === 0 && minute > 0 && minute < 45) return "00";
+
+  // if HH is 0 and MM is between HH:45:00 - HH:59:59 --> return String("01")
+  if (hour === 0 && minute >= 45 && minute < 60) return String("01");
+
+  // if HH is between 1 <--> 9 and MM is between HH:00:00 - HH: 44:59 --> return String("0") + same hour
+  if (hour > 0 && hour < 9 && minute > 0 && minute < 45)
+    return String("0" + hour);
+
+  // if HH is between 1 <-->9 and MM is between HH:45:00 - HH:59:59 --> return String("0") + next upcoming hour
+  if (hour > 0 && hour < 9 && minute >= 45 && minute < 60)
+    return String("0" + (hour + 1));
+
   // if HH is 9 and MM is between HH:45:59 - HH:59:59 --> return next upcoming hour
   if (hour === 9 && minute >= 45 && minute < 60) return "10";
 
@@ -10,9 +24,10 @@ const roundTimeHour = (hour, minute) => {
   if (hour === 9 && minute >= 0 && minute < 45) return "09";
 
   // if HH is not 9 and MM is between HH:00:00 - HH:44:59 --> return same hour
-  if (hour !== 9 && minute >= 0 && minute < 45) return String("0" + hour);
+  if (hour !== 9 && minute >= 0 && minute < 45) return String(hour);
+  // if (hour !== 9 && minute >= 0 && minute < 45) return String("0" + hour);
 
-  // // if HH is not 9 and MM is between HH:45:00 - HH:59:59 --> return next upcoming hour
+  // if HH is not 9 and MM is between HH:45:00 - HH:59:59 --> return next upcoming hour
   if (hour !== 9 && minute >= 45 && minute < 60) return String(hour + 1);
 };
 
